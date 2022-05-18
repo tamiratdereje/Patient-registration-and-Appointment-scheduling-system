5a73fde85047ac18611207b1176b9bb61cc5841c
Abel-Mekonen <abelmekonen559@gmail.com>
2022-05-18 15:00:18 +0300

    intro page modified

--- afyacare/lib/presentation/screens/intro_screen.dart
+++ afyacare/lib/presentation/screens/intro_screen.dart
@@ -1,6 +1,7 @@
-import 'package:afyacare/presentation/widgets/circleClip.dart';
+import 'package:afyacare/presentation/common/afya_theme.dart';
+import 'package:afyacare/presentation/widgets/circle_clip.dart';
+import 'package:afyacare/presentation/widgets/custom_button.dart';
 import 'package:afyacare/presentation_data/text_data.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class IntroScreen extends StatelessWidget {
@@ -12,42 +13,73 @@ class IntroScreen extends StatelessWidget {
       body: Stack(
         children: [
           const circleClip(),
-          Stack(
-            children: [
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [Text("AFYA"), Text("CARE")],
-                    ),
-                    Container(
-                        width: MediaQuery.of(context).size.width - 60,
-                        child: Image(image: AssetImage("Intro.png"))),
-                    // Container(
-                    //     width: MediaQuery.of(context).size.width - 60,
-                    //     decoration: const BoxDecoration(
-                    //       image: DecorationImage(
-                    //           image: AssetImage("assets/Intro.png")),
-                    //     )),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          TextData().introTitle,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Text(TextData().details)
-                      ],
-                    ),
-                    
-                  ],
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Stack(
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "AFYA ",
+                            style: AfyaTheme.lightTextTheme.headline1,
+                          ),
+                          Text(
+                            "CARE",
+                            style: AfyaTheme.lightTextTheme.headline1!
+                                .copyWith(color: Colors.green),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Container(
+                              width: MediaQuery.of(context).size.width - 120,
+                              child:
+                                  const Image(image: AssetImage("Intro.png"))),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                TextData().introTitle,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(TextData().details),
+                              const SizedBox(
+                                height: 30,
+                              ),
+                            ],
+                          ),
+                          const CustomButton(
+                              title: "Get Started",
+                              icon: Icons.arrow_forward_outlined),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 90,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           )
         ],
       ),
--- afyacare/lib/presentation/widgets/circle_clip.dart
+++ afyacare/lib/presentation/widgets/circle_clip.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 
-
 class ClipPathClass extends CustomClipper<Rect> {
   @override
   Rect getClip(Size size) {
-    return Rect.fromCircle(center: const Offset(180, 0), radius: 250);
+    return Rect.fromCircle(center: const Offset(180, -40), radius: 250);
   }
 
   @override
@@ -13,11 +12,10 @@ class ClipPathClass extends CustomClipper<Rect> {
   }
 }
 
-
 class ClipPathClass2 extends CustomClipper<Rect> {
   @override
   Rect getClip(Size size) {
-    return Rect.fromCircle(center: const Offset(400, 0), radius: 200);
+    return Rect.fromCircle(center: const Offset(400, -40), radius: 200);
   }
 
   @override
@@ -27,38 +25,34 @@ class ClipPathClass2 extends CustomClipper<Rect> {
 }
 
 class circleClip extends StatelessWidget {
-  const circleClip({ Key? key }) : super(key: key);
+  const circleClip({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Stack(
-        children: [
-          Container(
-
-            child: ClipOval(
-              clipper: ClipPathClass(),
-              child: SizedBox(
-                width:MediaQuery.of(context).size.width,
-                height:500,
-                child: Container(color: Colors.green.withOpacity(0.2)),
-              ),
+        child: Stack(
+      children: [
+        Container(
+          child: ClipOval(
+            clipper: ClipPathClass(),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 500,
+              child: Container(color: Colors.green.withOpacity(0.2)),
             ),
           ),
-
-           Container(
-
-            child: ClipOval(
-              clipper: ClipPathClass2(),
-              child: SizedBox(
-                width:MediaQuery.of(context).size.width,
-                height:250,
-                child: Container(color: Colors.green.withOpacity(0.2)),
-              ),
+        ),
+        Container(
+          child: ClipOval(
+            clipper: ClipPathClass2(),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+              child: Container(color: Colors.green.withOpacity(0.2)),
             ),
           ),
-        ],
-      
+        ),
+      ],
     ));
   }
 }
