8ce1be6a0a356fb097e4477860711b6176c23330
unknown <jebessadejene2021@gmail.com>
2022-05-25 18:58:30 +0300

    flutter set , signup page and appointment booking page

---
+++ afyacare/lib/presentation/widgets/brand_name.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import '../common/afya_theme.dart';
+
+class BrandName extends StatelessWidget {
+  const BrandName({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "AFYA ",
+          style: AfyaTheme.lightTextTheme.headline1!
+              .copyWith(fontWeight: FontWeight.normal),
+        ),
+        Text(
+          "CARE",
+          style: AfyaTheme.lightTextTheme.headline1!
+              .copyWith(color: Colors.green, fontWeight: FontWeight.w200),
+        )
+      ],
+    );
+  }
+}
