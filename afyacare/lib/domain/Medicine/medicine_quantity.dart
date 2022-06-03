d335cfe5f1f277a80a9a522722dd9febf7cddefc
unknown <jebessadejene2021@gmail.com>
2022-06-04 01:08:27 +0300

    add medicine unit test done

--- afyacare/lib/domain/Medicine/medicine_quantity.dart
+++ afyacare/lib/domain/Medicine/medicine_quantity.dart
@@ -2,11 +2,7 @@ import 'package:equatable/equatable.dart';
 
 class MedicineQuantity extends Equatable {
   String? medicineQuantity;
-  MedicineQuantity({required this.medicineQuantity}) {
-    if (this.medicineQuantity!.isEmpty || !RegExp(r'^[a-zA-Z . 0-9]+$').hasMatch(this.medicineQuantity!) ) {
-      throw Exception('Enter correct medicine quantity');
-    }
-  }
+  MedicineQuantity({required this.medicineQuantity});
 
   @override
   String toString() {
