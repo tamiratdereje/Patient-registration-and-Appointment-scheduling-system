5d9dae4f3a81c8532a54375e3ef54ed4fa46ba83
unknown <jebessadejene2021@gmail.com>
2022-06-03 23:37:01 +0300

    signup testing done

--- afyacare/lib/domain/signup/fullname_domain.dart
+++ afyacare/lib/domain/signup/fullname_domain.dart
@@ -4,7 +4,7 @@ class FullName extends Equatable {
   String? fullName;
   FullName({required this.fullName}) {
     if (this.fullName!.length < 3 || !RegExp(r'^[a-zA-Z .]+$').hasMatch(this.fullName!) ) {
-      throw Exception('Enter correct Name');
+      throw Exception('Enter correct name');
     }
   }
 
