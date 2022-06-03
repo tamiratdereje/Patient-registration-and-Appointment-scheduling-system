3b660aef1e4aa68ff1d3f2e86b793b8d9b3b8940
Tamirat Dereje <tamiratdereje167@gmail.com>
2022-06-03 14:27:47 +0300

    delete the schedule

--- afya_care_backend/src/controller/record_contoller.js
+++ afya_care_backend/src/controller/record_contoller.js
@@ -11,6 +11,7 @@ const add_record = async (req, res) => {
   if (error) return res.status(400).send({ message: error.details[0].message });
 
 
+  
   var record = new Record({
     date: req.body.date,
     descrption: req.body.descrption,
